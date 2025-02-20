df1fc057146e4c1d66abe2528368a96bf21c6c12
Margo63 <margo-vinograd@mail.ru>
2025-02-20 17:59:06 +0300

    test

--- app/src/main/java/com/example/garden/MainActivity.kt
+++ app/src/main/java/com/example/garden/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+
         setContent {
             GardenTheme {
                 SetUpOnGraph()
