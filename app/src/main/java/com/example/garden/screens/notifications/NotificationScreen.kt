0f5d6ad101392a573eb62c58719b244a2ac8e6e0
Starodubov Maxim <tofumaks@gmail.com>
2025-03-08 02:44:27 +0300

    NotificatonsScreen: fix notification card view

--- app/src/main/java/com/example/garden/screens/notifications/NotificationScreen.kt
+++ app/src/main/java/com/example/garden/screens/notifications/NotificationScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.icu.util.Calendar
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,11 +15,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -33,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -70,19 +75,19 @@ fun NotificationScreen(
         val notificationDate = it.dateStart.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
         val datesDifference = Period.between(notificationDate, currentDate)
 
-        return@filter datesDifference.days == 0 && it.title.contains(textInput)
+        return@filter datesDifference.days == 0 && it.title.lowercase().contains(textInput.lowercase())
     }
     val onWeekNotifications = dbViewModel.notifications.collectAsState().value.filter {
         val notificationDate = it.dateStart.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
         val datesDifference = Period.between(notificationDate, currentDate)
 
-        return@filter datesDifference.days < 7 && it.title.contains(textInput)
+        return@filter datesDifference.days < 7 && it.title.lowercase().contains(textInput.lowercase())
     }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(start = 10.dp, end = 10.dp, top = 15.dp)
+            .padding(start = 10.dp, end = 10.dp, top = 65.dp)
             .verticalScroll(rememberScrollState())
     ) {
         ChapterText(
@@ -138,23 +143,28 @@ fun NotificationCard(title: String, dateStart: Date, dateEnd: Date) {
     // TODO: is it ok?????
     val sdf = SimpleDateFormat("dd.MM.yyyy")
 
+    val onDeleteClick = { println("delete") }
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .shadow(elevation = 2.dp, shape = RoundedCornerShape(10.dp))
-            .padding(12.dp)
+            .background(color = Color.White)
     ) {
+        Icon(
+            imageVector = Icons.Default.Close,
+            null,
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth()
+                .size(15.dp)
+                .wrapContentWidth(Alignment.End)
+                .clickable(onClick = onDeleteClick)
+        )
         Column(
+            modifier = Modifier
+                .padding(12.dp)
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.cross),
-                null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentWidth(Alignment.End)
-            )
-        }
-        Column() {
             Text(
                 text = title,
                 fontSize = 13.sp
